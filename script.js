e2bbb328b6f4e1e62f36745da2113c37a5bf5066
Vince Andrew Loverez <155804641+vinceandrewloverez@users.noreply.github.com>
2024-02-29 22:00:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,12 @@ document.getElementById('colorButton').addEventListener('click', function() {
     index = (index + 1) % bodyColors.length;
 });
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5X017PR5LE"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'G-5X017PR5LE');
+</script>
